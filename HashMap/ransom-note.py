5faa6397e105e08d1fccc8a28b59ca73b530936c
TumsaUmata <tumsaumata2020@gmail.com>
2020-03-09 20:38:35 +0300

    Ransom Note

---
+++ HashMap/ransom-note.py
@@ -0,0 +1,8 @@
+def ransomNote(magazine, note):
+    # magazine = magazine.split(' ')
+    magazine =
+    print(magazine)
+
+
+if __name__ == '__main__':
+    ransomNote("give me one grand today night", "give one grand today")
