a50aab85694bc92d8c6034d29c888a0cea6ad0e5
TumsaUmata <tumsaumata2020@gmail.com>
2020-03-11 19:16:06 +0300

    Surrounded Regions Using BFS

--- Graph/surrounded_regions.py
+++ Graph/surrounded_regions.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Solution:
     def solve(self, board) -> None:
         if not board:
@@ -26,3 +29,34 @@ class Solution:
         self.dfs(board, row + 1, col)
         self.dfs(board, row, col - 1)
         self.dfs(board, row, col + 1)
+
+
+class Solution2:
+    def solve(self, board):
+        if not board:
+            return
+
+        queue = deque()
+        rows = len(board)
+        columns = len(board[0])
+
+        for row in range(rows):
+            for col in range(columns):
+                if (row == 0 or col == 0 or row == rows - 1 or col == columns - 1) and board[row][col] == "O":
+                    queue.append((row, col))
+
+        while queue:
+            row, col = queue.popleft()
+            board[row][col] = "T"
+            for x_axis, y_axis in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+                new_row = row + x_axis
+                new_col = col + y_axis
+                if 0 <= new_row < rows and 0 <= new_col < columns and board[new_row][new_col] == "O":
+                    queue.append((new_row, new_col))
+
+        for row in range(rows):
+            for col in range(columns):
+                if board[row][col] == "O":
+                    board[row][col] = "X"
+                elif board[row][col] == "T":
+                    board[row][col] = "O"
