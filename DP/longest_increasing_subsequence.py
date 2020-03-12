128f557ddfae9708ab9ef316a4fdda1047be6e90
TumsaUmata <tumsaumata2020@gmail.com>
2020-03-12 14:35:35 +0300

    Longest Increasing Subsequence

--- DP/longest_increasing_subsequence.py
+++ DP/longest_increasing_subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLIS(self, nums) -> int:
+        """O(N^2)"""
         if not nums:
             return 0
 
@@ -14,6 +15,32 @@ class Solution:
         return max(cache)
 
 
+class Solution2:
+    def lengthOfLIS(self, nums) -> int:
+        """O(NlogN)"""
+        subsequence = []
+        for num in nums:
+            position = self.binary_search(subsequence, num)
+            if position == len(subsequence):
+                subsequence.append(num)
+            else:
+                subsequence[position] = num
+        return len(subsequence)
+
+    def binary_search(self, subsequence, value: int) -> int:
+        left = 0
+        right = len(subsequence) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if subsequence[mid] < value:
+                left = mid + 1
+            elif subsequence[mid] > value:
+                right = mid - 1
+            else:
+                return mid
+        return left
+
+
 if __name__ == '__main__':
-    s = Solution()
+    s = Solution2()
     print(s.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
