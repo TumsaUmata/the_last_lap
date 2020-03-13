232e711efc4d2e4673b48e55d855a7ef23af9ccf
TumsaUmata <tumsaumata2020@gmail.com>
2020-03-13 15:47:44 +0300

    Min Abs Difference of Server Loads

---
+++ Google Assessment/Intern/min_abs_difference_of_server_loads.py
@@ -0,0 +1,22 @@
+def minAbs(nums):
+    s = sum(nums) // 2
+    n = len(nums)
+    dp = [[0 for _ in range(s + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, s + 1):
+            if nums[i - 1] <= j:
+                dp[i][j] = max(dp[i - 1][j], nums[i - 1] + dp[i - 1][j - nums[i - 1]])
+            else:
+                dp[i][j] = dp[i - 1][j]
+    return (sum(nums) - dp[-1][-1]) - dp[-1][-1]
+
+
+def main():
+    print(minAbs([1, 2, 3, 4, 5]))
+    print(minAbs([10, 10, 9, 9, 2]))
+    print(minAbs([]))
+    print(minAbs([1]))
+
+
+if __name__ == '__main__':
+    main()
